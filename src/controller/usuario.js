031b2f579bdc8bbe8e84f3e3abbe9850a2ef98e6
unknown <afonso@gmail.com>
2021-02-19 18:32:26 -0300

    atualização rotas

--- src/controller/usuario.js
+++ src/controller/usuario.js
@@ -1,8 +1,10 @@
-const { op } = require('sequelize');
+const { Op } = require('sequelize');
 const conexao = require('../database');
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { request } = require('express');
+const { update } = require('../models/Usuario');
 
 
 module.exports = {
@@ -17,31 +19,26 @@ module.exports = {
     },
 
     async store(request,response){
+
         const {nome,email} = request.body;
 
-        let usuario = await Usuario.findOne({
-            where:{
-                [op.or]:[
-                    {email:email}
-                ]
-            }
-        })
+        const roles = "user";
+
+        let usuario = await Usuario.findOne({where:{[Op.or]:[{nome:nome},{email:email}]}})
         if(usuario){
             return response.status(400).send({erro:"Usuario já cadastrado"})
         }
+            
+        let senha = Math.random().toString(36).slice(-10);
+        console.log('SENHA'+ senha);
 
-        function gerarpass(){
-            return Math.random().toString(36).slice(-10);
-        }
-
-        console.log(gerarpass);
-
-        //const secretPass = bcrypt.hash(50)
-        usuario = await Usuario.create({
-            nome,
-            email,
-            senha: secretPass
-        });
+        
+       try {
+           const secretPass = await bcrypt.hash(senha,10)
+          usuario = await Usuario.create({nome,email,senha:secretPass,roles});
+       } catch (error) {
+           console.log(error)
+       }
         response.status(201).send({
             usuario:{
                 usuario: usuario.idUser,
@@ -50,5 +47,28 @@ module.exports = {
                 senha : usuario.senha
             }
         })
+    },
+    async delete(req, res){
+        const usuario_id = req.usuarioId;
+
+        const { id } = req.params;
+
+        let usuario = await Usuario.findByPk( id );
+
+        if(!usuario){
+            return res.status(404).send({erro:"Usuario não encontrado"})
+        }
+
+        await usuario.destroy();
+
+        res.status(204).send({usuario: {IdDoUsuarioExcluido: usuario_id}})
+    },
+    async update(req, res){
+        const {nome,email,roles,senha,telefone,data_nasc} = req.params;
+        const {id} = req.params;
+        let usuario = await Usuario.findByPk(id)
+        //const senhaCript = await bcrypt.hash(senha, 10);
+        usuario = await usuario.update({nome, email, roles, senha, telefone, data_nasc})
+        res.status(201).send('atualizado')
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,7 @@ const usuarioController = require('./controller/usuario')
 
 routes.post("/usuario", usuarioController.store);
 routes.get('/usuario', usuarioController.list);
+routes.delete('/usuario/:id', usuarioController.delete);
+routes.put('/usuario/:id', usuarioController.update);
 
 module.exports = routes;
